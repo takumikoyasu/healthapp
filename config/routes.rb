aa014ea5f33c361ac47e18a0a7614ee4c21f6acd
takumikoyasu <63299205+takumikoyasu@users.noreply.github.com>
2020-06-02 10:29:10 +0900

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+Rails.application.routes.draw do
+  get 'questions/index'
+  get 'questions/new'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
